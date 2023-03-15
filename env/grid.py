fccd70532fa3ef3eb670e09d7ca3ec6fa740e141
minje-seok <minjeseok.tech@gmail.com>
2023-03-15 23:57:22 +0900

    feat: create class GridEnv()

--- env/grid.py
+++ env/grid.py
@@ -1,1 +1,8 @@
-# gird world example
+# minje-seok/Git-RL/env
+import torch
+
+class GridEnv():
+    def __init__(self):
+        self.state = torch.tensor([-1, -1])
+        self.action = -1
+        self.reward = 0
