669c1d9eeb7048443951a8261e574034a16e552f
minje-seok <minjeseok.tech@gmail.com>
2023-03-18 00:55:05 +0900

    feat: complete step function

--- env/grid.py
+++ env/grid.py
@@ -4,9 +4,9 @@ github.com/minje-seok/Git-RL/env/grid.py
 "Environment - Grid"
 
 [[ init, 0, 0, 0 ],
+[ 0, -1, 0, 0 ],
 [ 0, 0, 0, 0 ],
-[ 0, 0, 0, 0 ],
-[ 0, 0, 0, dst ]]
+[ 0, 0, -1, dst ]]
 
 '''
 import torch
@@ -20,31 +20,91 @@ class GridEnv():
         self.state = np.array([[1, 0, 0, 0],
                                [0, -1, 0, 0],
                                [0, 0, 0, 0],
-                               [0, -1, 0, 99]])
+                               [0, 0, -1, 99]])
         self.action = -1
         self.reward = 0
 
 
     def step(self, action):
         x, y = np.where(self.state == 1)
-        print(x, y)
         pos_x = int(x)
         pos_y = int(y)
-        print(pos_x, pos_y)
         if action == 'r':
-            if pos_x < self.state.shape[0] | self.state[pos_x][pos_y+1] != -1:
-                self.state[pos_x + 1] = 1
-                self.state[pos_x] = 0
+            if self.state[pos_x][pos_y + 1] == -1:
                 self.reward -= 1
+                return self.state, self.reward
+
+            elif pos_y < (self.state.shape[0]-1):
                 if self.state[pos_x][pos_y] == 99:
-                    self.reward += 1
-                    return
-            else:
+                    self.reward += 99
+                    return self.state, self.reward
+
+                self.state[pos_x][pos_y + 1] = 1
+                self.state[pos_x][pos_y] = 0
                 self.reward -= 1
-                return
+                return self.state, self.reward
+
+        elif action == 'l':
+            if self.state[pos_x][pos_y - 1] == -1:
+                self.reward -= 1
+                return self.state, self.reward
+
+            elif pos_y > 0:
+                if self.state[pos_x][pos_y] == 99:
+                    self.reward += 99
+                    return self.state, self.reward
+
+                self.state[pos_x][pos_y - 1] = 1
+                self.state[pos_x][pos_y] = 0
+                self.reward -= 1
+                return self.state, self.reward
+
+        if action == 'u':
+            if self.state[pos_x - 1][pos_y] == -1:
+                self.reward -= 1
+                return self.state, self.reward
+
+            elif pos_x > 0:
+                if self.state[pos_x][pos_y] == 99:
+                    self.reward += 99
+                    return self.state, self.reward
+
+                self.state[pos_x - 1][pos_y] = 1
+                self.state[pos_x][pos_y] = 0
+                self.reward -= 1
+                return self.state, self.reward
+
+        elif action == 'd':
+            if self.state[pos_x + 1][pos_y] == -1:
+                self.reward -= 1
+                return self.state, self.reward
+
+            elif pos_x < (self.state.shape[0] - 1):
+                if self.state[pos_x + 1][pos_y] == 99:
+                    self.reward += 99
+                    return self.state, self.reward
+
+                self.state[pos_x + 1][pos_y] = 1
+                self.state[pos_x][pos_y] = 0
+                self.reward -= 1
+                return self.state, self.reward
+
+    def cur_state(self):
+        print('------------')
+        print(self.state, 'reward: ',self.reward)
 
 env = GridEnv()
-
-for i in range(3):
-    env.step('r')
-    print(env.state)
+print()
+for i in range(1):
+    next_state, reward = env.step('r')
+    print(next_state, 'reward: ', reward)
+    next_state, reward = env.step('r')
+    print(next_state, 'reward: ', reward)
+    next_state, reward = env.step('r')
+    print(next_state, 'reward: ', reward)
+    next_state, reward = env.step('d')
+    print(next_state, 'reward: ', reward)
+    next_state, reward = env.step('d')
+    print(next_state, 'reward: ', reward)
+    next_state, reward = env.step('d')
+    print(next_state, 'reward: ', reward)
