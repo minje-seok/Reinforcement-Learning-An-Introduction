bfefb08da6b4a1f170e0f8bebb79c1c7d827ae69
minje-seok <minjeseok.tech@gmail.com>
2023-03-23 00:17:44 +0900

    refactor: agent to DP

--- agent.py
+++ agent.py
@@ -5,21 +5,22 @@ from env.grid import GridEnv
 
 env = GridEnv()
 
-class agent:
-    def __init__(self, env):
-        self.state = np.zeros(4,4)
-        self.next_state = self.state
-        self.action = -1
-        self.reward = 0
+class DP:
+    def __init__(self):
+        self.tabular = np.zeros(4, 4)
+        self.next_tabular = self.tabular
         self.env = env
 
     def iteration_step(self):
-        for i in range(self.state.shape[0]):
-            for j in range(self.state.shape[1]):
-                self.next_state[i, j] = 0.25 * (env.step(self.state[i, j], 'r') + self.state[i, j]) + \
-                                   0.25 * (env.step(self.state[i, j], 'l') + self.state[i, j]) + \
-                                   0.25 * (env.step(self.state[i, j], 'u') + self.state[i, j]) + \
-                                   0.25 * (env.step(self.state[i, j], 'd') + self.state[i, j])
+        for i in range(self.tabular.shape[0]):
+            for j in range(self.tabular.shape[1]):
+                self.next_state[i, j] = 0.25 * (env.step('r') + self.state[i, j]) + \
+                                   0.25 * (env.step('l') + self.state[i, j]) + \
+                                   0.25 * (env.step('u') + self.state[i, j]) + \
+                                   0.25 * (env.step('d') + self.state[i, j])
 
         self.state = self.next_state
         return self.state
+
+agent = DP()
+print(agent.state)
