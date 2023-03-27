646efc8554b24677d49a732613fcf79b52203540
cnrl-desktop <minje-seok.tech@gmail.com>
2023-03-27 16:23:46 +0900

    refactor: rivise policy_evalutaion() Add gamma, delta, theta to check convergence. Then, state-value function finally converge to integer at iteration 426.

--- agent.py
+++ agent.py
@@ -7,35 +7,46 @@ from env.grid import GridEnv
 
 class DP:
     def __init__(self):
-        self.state_V = np.zeros((4, 4))
-        self.next_state_V = np.zeros((4, 4))
+        self.V = np.zeros((4, 4))
+        self.next_V = self.V.copy()
         self.env = GridEnv()
 
-    def policy_evaluation(self):
-        for i in range(self.state_V.shape[0]):
-            for j in range(self.state_V.shape[1]):
-                if (i == 0 and j == 0) or (i == 3 and j == 3):
-                    continue
+    def policy_evaluation(self, gamma=1.0, theta=0.0000000001):
+        # gamma: discount factor
+        # theta: threshold to stop iteration
+        # delta: difference between V[i][j] and next_V[i][j](= new_V)
+        iter = 0
+        while True:
+            iter += 1
+            delta = 0
+            for i in range(self.V.shape[0]):
+                for j in range(self.V.shape[1]):
+                    if (i == 0 and j == 0) or (i == 3 and j == 3):
+                        continue
 
-                next_state_V = 0
-                for a in ['u', 'd', 'l', 'r']:
-                    self.env.create_grid(i, j)
-                    next_state, reward, done = self.env.step(a)
-                    x, y = np.where(next_state == 1)
-                    pos_x = int(x)
-                    pos_y = int(y)
-                    next_state_V += 0.25 * (reward + self.state_V[pos_x][pos_y])
-                    self.env.reset()
-                self.next_state_V[i][j] = next_state_V
-        self.state_V = self.next_state_V.copy()
+                    new_V = 0
+                    for a in ['u', 'd', 'l', 'r']:
+                        self.env.create_grid(i, j)
+                        next_state, reward, done = self.env.step(a)
+                        x, y = np.where(next_state == 1)
+                        pos_x = int(x)
+                        pos_y = int(y)
+                        new_V += 0.25 * (reward + gamma * self.V[pos_x][pos_y])
+                        self.env.reset()
+                    self.next_V[i][j] = new_V
+                    delta = max(delta, abs(self.V[i][j] - new_V))
+            self.V = self.next_V.copy()
 
-    def show(self):
-        print(self.next_state_V)
+            if delta < theta:
+                print('Policy Evaluation - iter:{0}'.format(iter))
+                print(self.next_V)
+                break
+
+    def policy_improvement(self):
+
+        return
+
 
 agent = DP()
 agent.policy_evaluation()
-agent.show()
-agent.policy_evaluation()
-agent.show()
-agent.policy_evaluation()
-agent.show()
+
